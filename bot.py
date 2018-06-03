176034e54d8a60446e17f3e5837b3aa896ca7eff
mraco <36960271+mraco@users.noreply.github.com>
2018-06-03 00:10:13 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,4 +56,4 @@ async def on_message(message):
                                                         dothething[str(message.server.id)]=0
                                 else:
                                         await asyncio.sleep(0.1)
-client.(os.environ['BOT_TOKEN'])
+client.run(os.environ['BOT_TOKEN'])
